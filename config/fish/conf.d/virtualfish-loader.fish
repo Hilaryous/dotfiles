87aa80ade00fd8c99207070dec4687e9333aa80d
Hilary Denton <hilaryous.denton@gmail.com>
2020-10-01 15:41:44 -0600

    Add virtualfish

---
+++ config/fish/conf.d/virtualfish-loader.fish
@@ -0,0 +1,4 @@
+set -g VIRTUALFISH_VERSION 2.5.0
+set -g VIRTUALFISH_PYTHON_EXEC /Users/hilarydenton/.pyenv/versions/3.8.5/bin/python3.8
+source /Users/hilarydenton/.pyenv/versions/3.8.5/lib/python3.8/site-packages/virtualfish/virtual.fish
+emit virtualfish_did_setup_plugins
