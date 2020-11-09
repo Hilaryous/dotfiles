5c721091f75d737a48912c50df31369bc3efb6a1
Hilary Denton <hilaryous.denton@gmail.com>
2020-11-09 13:06:27 -0700

    Update python version

--- config/fish/conf.d/virtualfish-loader.fish
+++ config/fish/conf.d/virtualfish-loader.fish
@@ -1,4 +1,4 @@
 set -g VIRTUALFISH_VERSION 2.5.0
-set -g VIRTUALFISH_PYTHON_EXEC /Users/hilarydenton/.pyenv/versions/3.8.5/bin/python3.8
-source /Users/hilarydenton/.pyenv/versions/3.8.5/lib/python3.8/site-packages/virtualfish/virtual.fish
+set -g VIRTUALFISH_PYTHON_EXEC /Users/hilary.denton/.pyenv/versions/3.9.0/bin/python
+source /Users/hilary.denton/.pyenv/versions/3.9.0/lib/python3.9/site-packages/virtualfish/virtual.fish
 emit virtualfish_did_setup_plugins
