ede4b2fa93891a15fc9918f403223fc098ff57bd
Hilary Denton <5042071+Hilaryous@users.noreply.github.com>
2024-06-20 12:43:34 -0600

    Updates after rerun in 6-2024

--- scripts/mac-pre-install.sh
+++ scripts/mac-pre-install.sh
@@ -3,16 +3,21 @@
 if [ ! -f "`which brew`" ]; then
   echo 'Installing homebrew'
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  (echo; echo 'eval "$(/usr/local/bin/brew shellenv)"') >> ~/.zprofile
-  eval "$(/usr/local/bin/brew shellenv)"
+
+  echo 'Run post install'
+  eval "$(opt/homebrew/bin/brew shellenv)"
+  (echo; echo 'eval "$(opt/homebrew/bin/brew shellenv)"') >> ~/.zprofile
   # brew tap homebrew/bundle  # Install Homebrew Bundle
 else
   echo 'Updating homebrew'
   brew update
 fi
 
-# TODO: figure how too skip is fish is already shell
 # Change default shell to bash and setup fish
-echo 'Changing default shell to fish, this will require a password'
-echo /usr/local/bin/fish | sudo tee -a /etc/shells
-chsh -s /usr/local/bin/fish
+if [ "$SHELL" != "/opt/homebrew/bin/fish" ]; then
+  echo 'Changing default shell to fish, this will require a password'
+  echo /opt/homebrew/bin/fish | sudo tee -a /etc/shells
+  chsh -s /opt/homebrew/bin/fish
+else
+  echo "Your shell is already set to fish (/opt/homebrew/bin/fish), skipping switching shell to fish"
+fi
