acc8b65f6d04c81ba14ac93c23350ca491743b91
Hilary Denton <5042071+Hilaryous@users.noreply.github.com>
2026-02-23 15:57:27 -0700

    fix git aliases

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -40,6 +40,26 @@ if type -q nvim
   alias vimdiff="nvim -d"
 end
 
+# Git aliases (g-prefix shortcuts; same subcommands are in ~/.gitconfig as git st, git co, etc.)
+if type -q git
+  alias ga='git add'
+  alias gap='git add -p'
+  alias gb='git branch'
+  alias gc='git commit'
+  alias gchead='git commit --amend -CHEAD'
+  alias gco='git checkout '
+  alias gcp='git cherry-pick'
+  alias glod='git log --graph --pretty="%Cgreen%h%Creset%Cblue%d%Creset %Cred%an%Creset: %s"'
+  alias glot='git log --no-merges --pretty=oneline --pretty="format :%h %s"'
+  alias gnap='git add -N . && git add -p'
+  alias gp='git push'
+  alias gpr='git pull --rebase'
+  alias gra='git rebase --abort'
+  alias grc='git rebase --continue'
+  alias gri='git rebase --interactive'
+  alias gst='git status'
+  alias reset-authors='git commit --amend --reset-author -C HEAD'
+end
 
 alias pg_start="launchctl load ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist"
 alias pg_stop="launchctl unload ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist"
